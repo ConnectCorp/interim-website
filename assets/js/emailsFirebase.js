118ae8f62affcd566e648aec3da72d901217bffd
rrtigga <rrtigga@ucdavis.edu>
2016-07-20 16:31:31 -0700

    able to store emails with both buttons, I need to make an error state though, also using the developer@connect.com google firebase account

--- assets/js/emailsFirebase.js
+++ assets/js/emailsFirebase.js
@@ -1,20 +1,19 @@
 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyASjsFqPZzHJOxy1O05UGDAgpaNN7_zD4k",
-    authDomain: "connect-emails-d89fa.firebaseapp.com",
-    databaseURL: "https://connect-emails-d89fa.firebaseio.com",
-    storageBucket: "connect-emails-d89fa.appspot.com",
+    apiKey: "AIzaSyASd4cVvOXK-XwNKXQsy46N97vj1dn5r2w",
+    authDomain: "connect-emails-7f29a.firebaseapp.com",
+    databaseURL: "https://connect-emails-7f29a.firebaseio.com",
+    storageBucket: "connect-emails-7f29a.appspot.com",
   };
   firebase.initializeApp(config);
 
 var email;
 
-function writeEmail(userEmail) {
-
+function writeFirstEmail(userEmail) {
   var firstButton = document.getElementById("firstButton");
 	userEmail = document.getElementById("firstEmail").value;
 	console.log(userEmail);
-	// Get a key for a new Post.
+	// Get a key for a new email.
 	var newEmailKey = firebase.database().ref().child('emails').push().key;
   
   if(validateEmail(userEmail)){
@@ -29,6 +28,28 @@ function writeEmail(userEmail) {
     console.log(userEmail + " is not a valid email.");
   }
 }
+
+function writeSecondEmail(userEmail) {
+  var secondButton = document.getElementById("secondButton");
+  userEmail = document.getElementById("secondEmail").value;
+  console.log(userEmail);
+  // Get a key for a new email.
+  var newEmailKey = firebase.database().ref().child('emails').push().key;
+  
+  if(validateEmail(userEmail)){
+    firebase.database().ref('emails/'+ newEmailKey ).set(userEmail);
+    console.log(userEmail + " saved in database");
+    
+    secondButton.style.background ='#1DB954';
+    secondButton.innerHTML = "🔥"+"  "+"We got it!"
+    secondButton.disabled = true; 
+  }
+  else{
+    console.log(userEmail + " is not a valid email.");
+  }
+}
+
+
 
 function validateEmail(email) {
   var re = /\S+@\S+\.\S+/;
