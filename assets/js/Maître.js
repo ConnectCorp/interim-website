2f574554382a1d43ac29a07136aa136dbb7b7136
rrtigga <rrtigga@ucdavis.edu>
2016-08-12 15:25:16 -0700

    added people behind via jquery, random int

--- assets/js/Maître.js
+++ assets/js/Maître.js
@@ -1,1 +1,18 @@
 //Maître.js
+
+function randomIntFromInterval(min,max)
+{
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+window.Maitre = {
+	uuid: "MFea521e2e4a",
+	callbacks: {
+	  afterSuccess: function(data, form) {
+	  	$('h4#maitre-people-behind-number').append(randomIntFromInterval(200,2400));
+	  	// $('div#maitre-content.show').append('<a class="close" href="#" rel="modal:close">Close</a>');
+	  	console.log("behind number added");
+	  }
+	}
+};
+
