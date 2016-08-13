f63ab29843c599c5541e71e4bc2702a12bc7961f
rrtigga <rrtigga@ucdavis.edu>
2016-08-12 20:37:58 -0700

    edited h4 text and added social icons

--- assets/js/Maître.js
+++ assets/js/Maître.js
@@ -9,6 +9,8 @@ window.Maitre = {
 	uuid: "MFea521e2e4a",
 	callbacks: {
 	  afterSuccess: function(data, form) {
+	  	$('div#maitre-content.show').addClass('modal');
+	  	$('div#maitre-content.show').modal();
 	  	$('h4#maitre-people-behind-number').append(randomIntFromInterval(200,2400));
 	  	// $('div#maitre-content.show').append('<a class="close" href="#" rel="modal:close">Close</a>');
 	  	console.log("behind number added");
