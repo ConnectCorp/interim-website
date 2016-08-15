de3f31e30f93d91164d17208d2f466e40ed8237c
rrtigga <rrtigga@ucdavis.edu>
2016-08-15 11:46:34 -0700

    added styling for maitre form

--- assets/js/Maître.js
+++ assets/js/Maître.js
@@ -11,7 +11,7 @@ window.Maitre = {
 	  afterSuccess: function(data, form) {
 	  	$('div#maitre-content.show').addClass('modal');
 	  	$('div#maitre-content.show').modal();
-	  	$('h4#maitre-people-behind-number').append(randomIntFromInterval(200,2400));
+	  	// $('h4#maitre-people-behind-number').append(randomIntFromInterval(200,2400));
 	  	// $('div#maitre-content.show').append('<a class="close" href="#" rel="modal:close">Close</a>');
 	  	console.log("behind number added");
 	  }
